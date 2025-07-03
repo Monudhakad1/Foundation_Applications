1e76460d4080b6a12b19b31d3d9c29811fabd4b0
Monudhakad1 <monudhakad055@gmail.com>
2025-07-03 22:50:18 +0530

    Added templates

---
+++ src/main/java/com/crudapp/strongfoundation_crudapp/StrongFoundationCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.crudapp.strongfoundation_crudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrongFoundationCrudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrongFoundationCrudAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crudapp/strongfoundation_crudapp/StrongFoundationCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudapp.strongfoundation_crudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrongFoundationCrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
